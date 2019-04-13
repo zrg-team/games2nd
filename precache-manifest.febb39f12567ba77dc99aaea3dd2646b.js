fd0cfd461a254cd5814a413bc1ebcf5537de4bd3
Hungdt-ibl <hungdt@blockchainlab.asia>
2019-04-14 00:38:21 +0700

    Updates

---
+++ precache-manifest.febb39f12567ba77dc99aaea3dd2646b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "8374767b8fea19ef90c9",
+    "url": "/static/css/main.d63f05b5.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6684f3589c924acf182ad5dccccba156",
+    "url": "/static/media/no-image.6684f358.jpg"
+  },
+  {
+    "revision": "6a2860f2f379a99abe47",
+    "url": "/static/js/2.6a2860f2.chunk.js"
+  },
+  {
+    "revision": "8374767b8fea19ef90c9",
+    "url": "/static/js/main.8374767b.chunk.js"
+  },
+  {
+    "revision": "dab664d9ed11f8835755609fadaf2bcc",
+    "url": "/static/media/chat-icon.dab664d9.png"
+  },
+  {
+    "revision": "3d5454114de2c70c54b117099814d2b8",
+    "url": "/static/media/main-banner.3d545411.jpg"
+  },
+  {
+    "revision": "a4d1b5aedb78dab6fa6184297eeb1de0",
+    "url": "/static/media/no-image-icon.a4d1b5ae.png"
+  },
+  {
+    "revision": "6a2860f2f379a99abe47",
+    "url": "/static/css/2.d67c03fe.chunk.css"
+  },
+  {
+    "revision": "37985e01127f2c1135ead912e9fc23ee",
+    "url": "/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,61 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.febb39f12567ba77dc99aaea3dd2646b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
+
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-messaging.js')
+
+/*
+ * Now initialize firebase app in the servie worker.
+ */
+firebase.initializeApp({
+  apiKey: 'AIzaSyC-TdnrW92tPP3_n7oo8netffqH5aJF_0o',
+  authDomain: 'game-store-zrg-team.firebaseapp.com',
+  databaseURL: 'https://game-store-zrg-team.firebaseio.com',
+  projectId: 'game-store-zrg-team',
+  storageBucket: 'game-store-zrg-team.appspot.com',
+  messagingSenderId: '790266601902'
+})
+
+var messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  var notificationTitle = payload.data.title
+  var notificationOptions = {
+    body: payload.data.body,
+    icon: payload.data.icon
+  }
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions)
+})
